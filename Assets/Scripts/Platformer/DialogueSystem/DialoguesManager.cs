1cd66cb8006c0b256220e2ffc191dbfc6f93c59a
Veselator <Veselator@DESKTOP-PM7KINB>
2025-11-18 09:49:45 +0200

    Litte bugfix before build

--- Assets/Scripts/Platformer/DialogueSystem/DialoguesManager.cs
+++ Assets/Scripts/Platformer/DialogueSystem/DialoguesManager.cs
@@ -1,6 +1,5 @@
 using System;
 using TMPro;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
--- Assets/Scripts/Quest/QuestObjectRegistry.cs
+++ Assets/Scripts/Quest/QuestObjectRegistry.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class QuestObjectRegistry : MonoBehaviour
